8d6a5b1bcdd9f416c32a488ccf7f891289a46c84
LI Daobing <lidaobing@gmail.com>
2014-02-12 14:21:19 +0800

    init capistrano-subset-fix

---
+++ lib/capistrano/subset/fix.rb
@@ -0,0 +1,9 @@
+require "capistrano/subset/fix/version"
+
+module Capistrano
+  module Subset
+    module Fix
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/capistrano/subset/fix/version.rb
@@ -0,0 +1,7 @@
+module Capistrano
+  module Subset
+    module Fix
+      VERSION = "0.0.1"
+    end
+  end
+end
